bc44ecd360e62214cb1c8c58ad8338e8d9f776cf
Fajar Fathurrozak <fajarfat22@if.unjani.ac.id>
2025-09-10 12:09:40 +0700

    Fixing Vercel

--- build.sh
+++ build.sh
@@ -13,13 +13,5 @@ php artisan event:cache
 npm install
 npm run build
 
-# 4. PENTING: Pindahkan direktori storage yang dibutuhkan ke /tmp
-# Vercel hanya mengizinkan penulisan ke direktori /tmp
-mv storage/framework /tmp/framework
-mv storage/logs /tmp/logs
-
-# 5. Buat symlink dari public/storage ke /tmp
-# Ini penting jika Anda menggunakan `php artisan storage:link`
-# (Hapus jika tidak relevan)
-# rm -rf public/storage
-# ln -s /tmp/storage public/storage
+# 4. Run database migrations
+php artisan migrate --force
